1e33ec866a5cd297394ca435634756b68fa7d4de
kingbabo <love.malmros@gmail.com>
2019-05-21 12:41:44 +0200

    plot

--- evaluate_model.py
+++ evaluate_model.py
@@ -3,12 +3,47 @@ from keras.models import model_from_json
 import json
 import pickle
 import numpy as np
+import itertools
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import seaborn as sn
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
 with open('model_in_json.json','r') as f:
@@ -40,15 +75,20 @@ for i, val in enumerate(y_test[14]):
 print(y_test.shape)
 print(prediction.shape)
 prediction = (prediction > 0.2)
-y_test = (y_test > 0.5)
 
 matrix = confusion_matrix(y_test.argmax(axis=1), prediction.argmax(axis=1))
+
+plt.figure()
+plot_confusion_matrix(matrix, classes=labels,
+                      title='Confusion matrix, without normalization')
+plt.show()
+"""
 print(matrix)
 df_cm = pd.DataFrame(matrix, index = [i for i in labels[:500]],
                   columns = [i for i in labels[:500]])
 plt.figure(figsize = (10,7))
 sn.heatmap(df_cm, annot=True)
-
+"""
 """
 conf_mat_dict={}
 
